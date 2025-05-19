297af8ed43ba6c374fef44b8eda124e117c1617f
kevinking1983 <156524630+kevinking1983@users.noreply.github.com>
2025-05-20 03:11:26 +0530

    Add files via upload

---
+++ 16)NPM/notes.js
@@ -0,0 +1,31 @@
+/*
+
+npm package repositories stores and manages the open source packages
+
+nearly 1million packages frameworks,etc
+
+init command to create a package.json file
+
+=> Packages classification
+
+regular dependency - Application or code depends on package
+
+development dependency - working of our application does not depend but only for development purposes(mention --save-dev at end when installing)
+
+npm install __(package name)__ @ __(version)__
+                                 [only if you want a specific version]
+
+=> Types of installation
+
+local installation: The package is only available in that folder where the package is installed
+
+global installation: The package can be accesed from any folder or project directory (-g/-global at end of install/i)
+  
+npm install -g nodemon --save--dev
+
+
+
+
+
+
+*/
